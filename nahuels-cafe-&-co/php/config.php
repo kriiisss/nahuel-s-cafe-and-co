10b18d2dd3011e0542b26d7fa49713caa2481d44
kriiisss <91886966+kriiisss@users.noreply.github.com>
2023-11-07 21:26:53 -0300

    Delete nahuels-cafe-&-co directory

--- nahuels-cafe-&-co/php/config.php
+++
@@ -1,3 +0,0 @@
-<?php
-define ("KEY_TOKEN", "4gR0P3cu4RiO")
-?>
--- nahuels-cafe-&-co/php/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-    include 'php/connection.php';
-
-    $instruccion="insert into users(name, surname, dni, fecha_nacimiento, phone_number, email, username, password) values ('name', 'surname', 'dni', 'fecha_nacimiento', 'phone_number', 'email', 'username', 'password')";
-    mysqli_close ($connection);
-?>
