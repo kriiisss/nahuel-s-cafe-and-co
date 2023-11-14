a6a9d007ed71c4d811be1f9fd8ddb050a44c1a9e
kriiisss <91886966+kriiisss@users.noreply.github.com>
2023-11-14 19:07:53 -0300

    Add files via upload

---
+++ nahuels-cafe-&-co/php/login.php
@@ -0,0 +1,25 @@
+<?php
+require 'connection.php';
+$user = $_GET['user'];
+$pass = md5($_GET['pass']);
+
+if(isset($user) && isset($pass)){
+    $db = new Database();
+    $conn = $db->conectar();
+    $statement = $conn->prepare("SELECT * FROM users WHERE username = ? and password = ?");
+
+    $statement->execute(array($user, $pass));
+
+    if($row = $statement->fetch()){
+        session_start();
+        header('location: ../inicio.html');
+        echo htmlspecialchars($user);           
+    }else{
+        echo "Nombre de usuario y/o contraseña incorrectos";
+    }
+}
+
+//' or 1 = 1; #
+//<script>location.href="https://google.com"</script>
+
+?>
